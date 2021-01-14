e2609bc81403a9a57b5bfdfd80538cda1cfacac5
Yogesh <Yogesh@DESKTOP-RUAUPJL>
2021-01-14 18:57:49 +0530

    my first commit

---
+++ Accessmodifier/src/com/csi/protectedmodifier/Protectedmodifier.java
@@ -0,0 +1,13 @@
+package com.csi.protectedmodifier;
+
+import com.csi.AccessmodifiercoreConcept.Accessmodifier;
+
+public class Protectedmodifier extends Accessmodifier{
+
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
