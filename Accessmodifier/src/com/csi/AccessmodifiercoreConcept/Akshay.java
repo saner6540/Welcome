20c134c9772d9c944874b335d57018a0c1fa2af4
Yogesh <Yogesh@DESKTOP-RUAUPJL>
2021-01-14 19:02:54 +0530

    2nd commit

---
+++ Accessmodifier/src/com/csi/AccessmodifiercoreConcept/Akshay.java
@@ -0,0 +1,11 @@
+package com.csi.AccessmodifiercoreConcept;
+
+public class Akshay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Test");
+	}
+
+}
